6eddcb5bb72b9a8de3d5f9f0fd5f1cdc6ab6d572
amber_k <amberstuntz@gmail.com>
2022-01-18 12:01:07 -0600

    Final adjustments

--- js/script.js
+++ js/script.js
@@ -31,8 +31,6 @@ const placeholder = function (word) {
     wordInProgress.innerText = placeholderLetters.join("");
 };
 
-placeholder(word);
-
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
     message.innerText = "";
@@ -92,6 +90,7 @@ const updateWordInProgress = function (guessedLetters) {
         }
     }
 
+    // console.log(revealWord);
     wordInProgress.innerText = revealWord.join("");
     checkIfWin();
 };
@@ -107,6 +106,7 @@ const updateGuessesRemaining = function (guess) {
 
     if (remainingGuesses === 0) {
         message.innerHTML = `Game over! The word was <span class="highlight">${word}</span>.`;
+        startOver();
     }else if (remainingGuesses === 1) {
         remainingGuessesSpan.innerText = `${remainingGuesses} guess`;
     } else {
@@ -118,5 +118,31 @@ const checkIfWin = function () {
     if (word.toUpperCase() === wordInProgress.innerText) {
         message.classList.add("win");
         message.innerHTML = `<p class= "highlight">You guessed the correct word! Congrats!</p>`;
-    }
+    
+        startOver();
+    }     
 };
+
+const startOver = function () {
+    guessLetterButton.classList.add("hide");
+    remainingGuessesElement.classList.add("hide");
+    guessedLettersElement.classList.add("hide");
+    playAgainButton.classList.remove("hide");
+};
+
+playAgainButton.addEventListener("click", function () {
+    //reset all original values - grab new word
+    message.classList.remove("win");
+    guessedLetters = [];
+    remainingGuesses = 8;
+    remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+    guessedLettersElement.innerHTML = "";
+    message.innerText = "";
+    //Grab a new word
+    getWord();
+
+    guessLetterButton.classList.remove("hide");
+    playAgainButton.classList.add("hide");
+    remainingGuessesElement.classList.remove("hide");
+    guessedLettersElement.classList.remove("hide");
+});
